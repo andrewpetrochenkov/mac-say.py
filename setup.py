f88dbadb5831c7c8f2afc5253e7cf1330c2ad3d1
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-04 00:07:52 +0300

    init

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mac-say',
+    version='2020.12.3',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
